03178aca38b20057c38aedccb14f11ce226a47d8
Kimberly Owens <kimberly.j.owens@gmail.com>
2020-08-17 22:25:40 -0600

    removing html route

--- public/service-worker.js
+++ public/service-worker.js
@@ -14,7 +14,7 @@ const DATA_CACHE_NAME = "data-cache-v1";
 self.addEventListener('install', function(evt) {
   evt.waitUntil(
     caches.open(CACHE_NAME).then(cache => {
-      console.log("Your files were pre-cached successfully!");
+      console.log('Your files were pre-cached successfully!');
       return cache.addAll(FILES_TO_CACHE);
     })
   );
